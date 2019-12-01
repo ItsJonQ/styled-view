1a6e85bda14a6e64fc0bd75edb73d02d77d8bbc0
Jon Q <hello@jonquach.com>
2019-11-30 19:33:37 -0500

    Add @itsjonq/is for type checking

--- src/BaseView.js
+++ src/BaseView.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { sanitizeStyleProps } from 'is-style-prop-valid';
-import { isPlainObject } from './utils';
+import { is } from './utils';
 
 const defaultProps = {
 	boxSizing: 'border-box',
@@ -10,7 +10,7 @@ export const BaseView = styled('div')(props => {
 	const mergedProps = { ...defaultProps, ...props };
 	const { sx, ...restProps } = mergedProps;
 
-	const stylePropsObject = isPlainObject(sx) ? sx : {};
+	const stylePropsObject = is.plainObject(sx) ? sx : {};
 	const styleProps = { ...restProps, ...stylePropsObject };
 
 	return sanitizeStyleProps(styleProps);
